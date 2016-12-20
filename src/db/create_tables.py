6b9265a5645eb7c0699967a48e3392c1c5073586
piercecunneen <pcunneen@nd.edu>
2016-12-20 00:54:05 -0500

    Setting up db

--- src/db/create_tables.py
+++ src/db/create_tables.py
@@ -1,14 +1,10 @@
 import psycopg2 as pg
 
+import db_lib
+
+
 def create_table(db_name, table_name, column_names, column_types):
-  conn = pg.connect("dbname=%s" %(db_name))
-  curr = conn.cursor()
-
   columns = ["%s %s" %(pair[0], pair[1]) for pair in zip(column_names, column_types)]
-  create_string = "CREATE TABLE %s (%s)" %(table_name, ', '.join(columns))
-
-  curr.execute(create_string)
-  conn.commit()
-
-  curr.close()
-  conn.close()
+  query_string = "CREATE TABLE %s (%s)" %(table_name, ', '.join(columns))
+  error = db_lib.execute_db_query(db_name, query_string, [])
+  return error
--- src/db/destroy_tables.py
+++ src/db/destroy_tables.py
@@ -1,13 +1,8 @@
 import psycopg2 as pg
 
+import db_lib
+
 def drop_table(db_name, table_name):
-  conn = pg.connect("dbname=%s" %(db_name))
-  curr = conn.cursor()
-
-  drop_string = "DROP TABLE %s" %(table_name)
-
-  curr.execute(drop_string)
-  conn.commit()
-
-  curr.close()
-  conn.close()
+  query_string = "DROP table %s" %(table_name)
+  error = db_lib.execute_db_query(db_name, query_string, [])
+  return error
