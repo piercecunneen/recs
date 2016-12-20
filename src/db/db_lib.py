750b4c370cd05949f272e994decff8f7ffe9b440
piercecunneen <pcunneen@nd.edu>
2016-12-20 13:00:46 -0500

    set up linter and get rid of initial lint errors

--- src/db/db_lib.py
+++ src/db/db_lib.py
@@ -1,15 +1,27 @@
+"""
+Set of commonly used database functions that are shared
+across the codebase
+"""
 import psycopg2 as pg
 
-
 def execute_db_query(db_name, query_string, args):
+  """
+  Inputs:
+    db_name: type(string)
+    query_string: type(string)
+    args: type(list of strings)
+  returns:
+    type:
+      pg.Error or NoneType)
+  """
   conn = pg.connect("dbname=%s" %(db_name))
   curr = conn.cursor()
 
   try:
     curr.execute(query_string, args)
     conn.commit()
-  except pg.OperationalError as e:
-    return e
+  except pg.OperationalError as err:
+    return err
 
   curr.close()
   conn.close()
--- src/db/users/create_user.py
+++ src/db/users/create_user.py
@@ -1,8 +1,21 @@
-import src.db.db_lib as db
+"""
+Takes a new user's informatio and inserts them into
+ the database
+
+"""
+import recs.src.db.db_lib as db
 
 def create_user(username, email):
-
+  """
+  Inputs:
+    username: type(string)
+    email:    type(string)
+  returns:
+    type:
+      pg.Error or NoneType)
+    description:
+      result of recs/src/db/db_lib.execute_db_query, which
+      is either an error or None (if query successful)
+  """
   query_string = "INSERT INTO users (username, email, verified) VALUES (%s, %s, %s)"
   return db.execute_db_query("piercecunneen", query_string, [username, email, "0"])
-
-create_user("pierce", "pdc")
