83985352db6d0e8a625df8f272fccf1c720780bc
piercecunneen <pcunneen@nd.edu>
2016-12-26 16:40:05 -0500

    updated request validation to check for types

--- scripts/validate.py
+++ scripts/validate.py
@@ -1,3 +1,8 @@
+'''
+Methods associated with request validation
+
+'''
+
 def validate_request(valid_body, request_body):
   '''
     validate response by comparing actual request body with the api validaiton body
@@ -5,11 +10,27 @@ def validate_request(valid_body, request_body):
   required_fields = {key:valid_body[key] for key in valid_body if valid_body[key]['required']}
   num_required_fields_seen = 0
   for key in request_body:
-    if key in required_fields:
+    if key in required_fields and type_match(valid_body[key]['type'], request_body[key]):
       num_required_fields_seen += 1
       continue
-    elif key in valid_body:
+    elif key in valid_body and type_match(valid_body[key]['type'], request_body[key]):
       continue
     else:
       return False
   return num_required_fields_seen == len(required_fields)
+
+
+def type_match(valid_body_string, field):
+  '''
+    Returns true if the field has the correct type
+  '''
+  if valid_body_string == "int":
+    return isinstance(field, int)
+  elif valid_body_string == "string" or valid_body_string == "char" and len(field) == 1:
+    return isinstance(field, str) or isinstance(field, unicode)
+  elif valid_body_string == "object":
+    return isinstance(field, dict)
+  elif valid_body_string == "list":
+    return isinstance(field, list)
+  else:
+    return True
