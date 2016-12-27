eb7699f847683300b39a9a0040858f3fd4f0ee0f
piercecunneen <pcunneen@nd.edu>
2016-12-27 00:29:22 -0500

    add primary and foreign key to create tables

--- src/db/create_tables.py
+++ src/db/create_tables.py
@@ -4,7 +4,7 @@ Most commonly used during setup stage
 """
 import src.db.db_lib as db
 
-def create_table(db_name, table_name, column_names, column_types):
+def create_table(db_name, table_name, column_names, column_types, foreign_keys, primary_keys):
   """
   Inputs:
     db_name: type(string)
@@ -19,7 +19,9 @@ def create_table(db_name, table_name, column_names, column_types):
       result of recs/src/db/db_lib.execute_db_query, which
       is either an error or None (if query successful)
   """
+  foreign_key_strings = ["FOREIGN KEY (%s) REFERENCES %s (%s)" %(fk['column'], fk['foreign_table'], fk['foreign_column']) for fk in foreign_keys]
+  primary_key_strings = ["PRIMARY KEY (%s)" %(key) for key in primary_keys]
   columns = ["%s %s" %(pair[0], pair[1]) for pair in zip(column_names, column_types)]
-  query_string = "CREATE TABLE %s (%s)" %(table_name, ', '.join(columns))
+  query_string = "CREATE TABLE %s (%s, %s, %s)" %(table_name, ', '.join(columns), ', '.join(foreign_key_strings), ', '.join(primary_key_strings))
   error = db.execute_db_query(db_name, query_string, [])
   return error
